fc1fa52d42e94c6fc79f6439de84202b0358b3a5
Martin <mr.wiklander@gmail.com>
2013-02-17 15:25:35 +0100

    Fixed double buffer

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,8 @@ main :: IO ()
 main = do
   (progname, _) <- getArgsAndInitialize
   createWindow "Hoids"
-  initialDisplayMode $= [DoubleBuffered]
+  initialDisplayMode $= [WithDepthBuffer, DoubleBuffered, RGBMode]
+  depthFunc  $= Just Less
   windowSize  $= (Size 680 400)
   windowPosition $= (Position 500 500)
   cursor $= None
@@ -27,21 +28,20 @@ frameUpdate = do
   flush
 
 bgColor :: Color4 GLfloat   
-bgColor = (Color4 1 (242/255) (229/255) 1)
+bgColor = (Color4 1 (242/255) (229/255) 0.5)
 
 display :: IO ()
 display = do
   clearColor $= bgColor
   clear [ ColorBuffer, DepthBuffer ]
-  renderPrimitive Polygon $ do
+  loadIdentity
+  preservingMatrix $ renderPrimitive Polygon $ do
         color $ (Color4 (0.0::GLfloat) 0 0 0.4)
         mapM_ (\(x, y, z)->vertex$Vertex3 x y z) $ circle 0.1 (0,0,-0.4)
-  loadIdentity
-  flush
   swapBuffers
 
 circle :: GLfloat -> (GLfloat,GLfloat,GLfloat) -> [(GLfloat,GLfloat,GLfloat)]
-circle r (x,y,z) = map (\n -> ( xCalc(n) * r * 0.588235294 + x, yCalc(n) * r + y,0.0 * r + z )) [1..nrOfLines]
+circle r (x,y,z) = map (\n -> ( xCalc(n) * r * 400/680 + x, yCalc(n) * r + y,0.0 * r + z )) [1..nrOfLines]
         where 
                 nrOfLines = 100
                 xCalc n = sin(2*pi*n/nrOfLines)
