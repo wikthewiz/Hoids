9a43f4fbd497822b99a544d4739a579b6fae0b67
Martin <mr.wiklander@gmail.com>
2013-02-19 22:04:36 +0100

    Start of adding world

--- src/Main.hs
+++ src/Main.hs
@@ -17,31 +17,47 @@ main = do
   displayCallback $= display world
   idleCallback $= Just (frameUpdate world)
   mainLoop
+
+boidRadious = 0.1
+
+createBoids :: Int -> Vector -> [Boid]
+createBoids 0 _ = []
+createBoids nrOfBoids prevPos =  let boid = createBoid
+                                 in  boid : createBoids (nrOfBoids - 1) (pos boid)
+      where
+        createBoid =  let x = prevPos
+                      in 
+                        Boid { 
+                              direction = Vector 0 0 0,
+                              pos = Vector x + boidRadious y + boidRadious z + boidRadious 
+                            } 
  
+
+createWorld nrOfBoids = World {boids = createBoids nrOfBoids $ Vector 0 0 0 }
 
 close :: IO ()
 close = do
-  putStrLn "exit"
-  flush
+        putStrLn "exit"
+        flush
 -- Use this function to move and impact the displaying.
 frameUpdate :: IORef GLfloat -> IO()
 frameUpdate world = do
-  postRedisplay Nothing
+        postRedisplay Nothing
 
 bgColor :: Color4 GLfloat   
 bgColor = (Color4 1 (242/255) (229/255) 0.5)
 
 display :: IORef GLfloat -> IO()
 display world = do
-  w <- get world
-  world $=! updateWorld(w)
-  clearColor $= bgColor
-  clear [ ColorBuffer, DepthBuffer ]
-  loadIdentity
-  preservingMatrix $ renderPrimitive Polygon $ do
-        color $ (Color4 (0.0::GLfloat) 0 0 0.4)
-        mapM_ (\(x, y, z)->vertex$Vertex3 x y z) $ circle (w*0.1) (0,0,-0.4)
-  swapBuffers
+        w <- get world
+        world $=! updateWorld(w)
+        clearColor $= bgColor
+        clear [ ColorBuffer, DepthBuffer ]
+        loadIdentity
+        preservingMatrix $ renderPrimitive Polygon $ do
+                color $ (Color4 (0.0::GLfloat) 0 0 0.4)
+                mapM_ (\(x, y, z)->vertex$Vertex3 x y z) $ circle (w*0.1) (0,0,-0.4)
+        swapBuffers
 
 circle :: GLfloat -> (GLfloat,GLfloat,GLfloat) -> [(GLfloat,GLfloat,GLfloat)]
 circle r (x,y,z) = map (\n -> ( xCalc(n) * r * 400/680 + x, yCalc(n) * r + y,0.0 * r + z )) [1..nrOfLines]
@@ -52,3 +68,9 @@ circle r (x,y,z) = map (\n -> ( xCalc(n) * r * 400/680 + x, yCalc(n) * r + y,0.0
 
 updateWorld :: Fractional a => a -> a
 updateWorld world = world + 0.001
+
+data Vector = Vector GLfloat GLfloat GLfloat deriving (Show)
+data Boid  = Boid { direction :: Vector
+                    , pos:: Vector 
+                    } deriving (Show)
+data World = World { boids :: [Boid] }  deriving (Show)
